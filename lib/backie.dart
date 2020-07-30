9e165beb8a8028ca8a910278a9dfd2cd56c8efac
nicoale <nicoale8786@gmail.com>
2020-07-30 18:37:00 +0300

    adds backup code

---
+++ lib/backie.dart
@@ -0,0 +1,85 @@
+
+// import 'package:flutter/material.dart';
+
+// class Body extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//          bottomNavigationBar: CurvedNavigationBar(
+//     backgroundColor: Colors.blueAccent,
+//     items: <Widget>[
+//       Icon(Icons.add, size: 30),
+//       Icon(Icons.list, size: 30),
+//       Icon(Icons.compare_arrows, size: 30),
+//     ],
+//     onTap: (index) {
+//       //Handle button tap
+//     },
+//   ),
+
+
+
+
+
+
+//       //   bottomNavigationBar:Curved(
+//       //   backgroundColor: Colors.white,
+//       //   color: Colors.deepPurple,
+//       //   buttonBackgroundColor: Colors.deepPurple,
+//       //   height: 60,
+//       //   animationDuration: Duration(
+//       //     milliseconds: 200,
+//       //   ),
+//       //   index: 2,
+//       //   animationCurve: Curves.bounceInOut,
+//       //   items: <Widget>[
+//       //     Icon(Icons.favorite, size: 30, color: Colors.white),
+//       //     Icon(Icons.verified_user, size: 30, color: Colors.white),
+//       //     Icon(Icons.home, size: 30, color: Colors.white),
+//       //     Icon(Icons.settings, size: 30, color: Colors.white),
+//       //     Icon(Icons.more_horiz, size: 30, color: Colors.white),
+//       //   ],
+//       //   onTap: (index) {
+//       //     //Handle button tap
+//       //   },
+//       // ),
+//         //  Column(children: <Widget>[
+//         //   //income or outcome
+//         //   Expanded(
+//         //       child: Stack(children: <Widget>[
+//         //     // background
+//         //     Container(
+//         //       height: 400,
+//         //       margin: EdgeInsets.only(top: 70),
+//         //       decoration: BoxDecoration(
+//         //         color: Colors.blueAccent,
+//         //       ),
+//         //     )
+//         //   ])),
+//         //   Expanded(
+//         //       child: Stack(children: <Widget>[
+//         //     // background
+//         //     Container(
+//         //       height: 400,
+//         //       margin: EdgeInsets.only(top: 70),
+//         //       decoration: BoxDecoration(
+//         //         color: Colors.white,
+//         //         borderRadius: BorderRadius.only(
+//         //           topLeft: Radius.circular(40),
+//         //           topRight: Radius.circular(40),
+//         //         ),
+//         //       ),
+//         //     ),
+//         //   ]
+//         // ))])
+//         );
+//   }
+// }
+ // physics: NeverScrollableScrollPhysics(),
+            // shrinkWrap: true,
+            // padding: EdgeInsets.all(8.0),
+            // crossAxisCount: 2,
+            // crossAxisSpacing: 1.0,
+            // mainAxisSpacing: 5.0,
+            // childAspectRatio: 1.0,
+          // ),
